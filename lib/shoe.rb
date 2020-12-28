f144b422302b1733fd2564d2ad1f5a8f0838978a
HELEN HARMAN <hlharman@bellsouth.net>
2020-12-28 18:58:43 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,8 @@ class Shoe
      
  
   attr_reader :brand
-  
+  attr_accessor :color, :size, :material, :condition
+ 
   def initialize(brand)
     @brand= brand 
   end 
@@ -10,8 +11,6 @@ class Shoe
 def brand 
     @brand 
   end 
-  
-  
   
   def cobble
     puts "Your shoe is as good as new!"
